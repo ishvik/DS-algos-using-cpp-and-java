ccfbded2c587050947ebd6edb88a777cc4b3b10c
simran <singh.simran9654@gmail.com>
2020-08-08 13:47:00 +0530

    chaining

--- Hashing/chaining.cpp
+++ Hashing/chaining.cpp
@@ -36,18 +36,40 @@ void sortedInsert(struct Node **H,int val)
   }
 }
 
-Node* search(Node *p,int key)
+bool search(Node *p,int key)
 {
   while(p!=NULL)
   {
     if(key == p->data)
-      return p;
+      return true;
     p=p->next;
   }
-  return NULL;
+  return false;
+}
+
+int hash1(int key)
+{
+  return key%10;
+}
+
+void insert(Node *H[],int key)
+{
+  int index;
+  index = key%10;
+  sortedInsert(&H[index],key);
 }
 
 int main()
 {
+  struct Node *HT[10];
+  for(int i=0;i<10;i++)
+    HT[i] = NULL;
+  for(int i=0;i<10;i++)
+  {
+    int x;
+    cin>>x;
+    insert(HT,x);
+  }
+  cout<<search(HT[hash1(2)],2);
   return 0;
 }
