4d9d16b530d8116c3a962402a722135757f9f6d1
simran <singh.simran9654@gmail.com>
2021-03-17 12:57:47 +0530

    Get Common Element 2

---
+++ HashMap/CommonElement2.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class CommonElement2{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner scn = new Scanner(System.in);
+    int n1 = scn.nextInt();
+    int []arr1 = new int[n1];
+    for(int i=0;i<arr1.length;i++){
+        arr1[i] = scn.nextInt();
+    }
+    
+    int n2 = scn.nextInt();
+    int []arr2 = new int[n2];
+    for(int i=0;i<arr2.length;i++){
+        arr2[i] = scn.nextInt();
+    }
+    
+    HashMap<Integer,Integer> map = new HashMap<>();
+    for(int i=0;i<arr1.length;i++){
+        int v = arr1[i];
+        if(map.containsKey(v)){
+            int val = map.get(v);
+            int n = val+1;
+            map.put(v,n);
+        }else{
+            map.put(v,1);
+        }
+    
+    }
+    
+    for(int i=0;i<n2;i++){
+        if(map.containsKey(arr2[i]) && map.get(arr2[i]) > 0) {
+            System.out.println(arr2[i]);
+            int pv = map.get(arr2[i]);
+            pv = pv-1;
+            map.put(arr2[i],pv);
+        }
+    }
+
+}
+}
